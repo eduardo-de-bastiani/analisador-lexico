1aa95e92d72ed0d6f5d29c17ef5e59366e05ac98
Erik Vieira Suris <erik.suris@edu.pucrs.br>
2024-09-03 21:52:41 -0300

    Finished states

--- ReconhecedorPalavrasReservadas.java
+++ ReconhecedorPalavrasReservadas.java
@@ -29,121 +29,166 @@ public class ReconhecedorPalavrasReservadas {
             char caractere = entrada.charAt(i);
             transicao(caractere);
         }
-        return estadoAtual  == ; 
+        return estadoAtual == 0; 
     }
-
-    
-
 
     private static int transicao(char caractere) {
         switch (estadoAtual) {
             case 0:
                 if (caractere == 'f') {
                     estadoAtual = 1;
-                } else if(caractere == 'i'){
+                } else if (caractere == 'i') {
                     estadoAtual = 2;
-                } else if(caractere == 'e'){
+                } else if (caractere == 'e') {
                     estadoAtual = 3;
-                } else if(caractere == 'w'){
+                } else if (caractere == 'w') {
                     estadoAtual = 4;
-                } else if(caractere == 'd'){
+                } else if (caractere == 'd') {
                     estadoAtual = 5;
-                } else if (caractere == 's'){
+                } else if (caractere == 's') {
                     estadoAtual = 6;
-                }
-                else {
-                    estadoAtual = -1; 
+                } else {
+                    return -1; 
                 }
                 break;
             case 1:
                 if (caractere == 'o') {
                     estadoAtual = 7;
-                } else if(caractere == 'l'){
+                } else if (caractere == 'l') {
                     estadoAtual = 8;
-                }else{
+                } else {
                     return -1;
                 }
                 break;
             case 2:
-                if (caractere == 'f'){
+                if (caractere == 'f') {
                     return 21;
-                } else if(caractere == 'n'){
+                } else if (caractere == 'n') {
                     estadoAtual = 9;
-                } 
-                else{
+                } else {
                     return -1;
                 }
+                break;
             case 3:
-                if(caractere == 'l'){
+                if (caractere == 'l') {
                     estadoAtual = 10;
-                }else{
+                } else{
                     return -1;
                 }
+                break;
             case 4:
-                if(caractere == 'h'){
+                if (caractere == 'h') {
                     estadoAtual = 11;
-                }else{
+                } else {
                     return -1;
                 }
+                break;
             case 5:
-                if(caractere == 'o'){
+                if (caractere == 'o') {
                     return 24;
+                } else {
+                    return -1;
                 }
             case 6:
-                if(caractere == 'w'){
+                if (caractere == 'w') {
                     estadoAtual = 12;
+                } else {
+                    return -1;
                 }
-            
+                break;
             case 7:
-                if(caractere == 'r'){
+                if (caractere == 'r') {
                     return 20;
-                }else{
+                } else {
                     return -1;
                 }
             case 8:
-                if(caractere == 'o'){
+                if (caractere == 'o') {
                     estadoAtual = 13;
-                }else{
+                } else {
                     return -1;
                 }
-            
+                break;
             case 9: 
-                if(caractere == 's'){
-                    estadoAtual = 10;
-                }else{
+                if (caractere == 't') {
+                    return 25;
+                } else {
                     return -1;
                 }
             case 10:
-                if(caractere == 'e'){
-                    return 22;
-                } else if(caractere == 't'){
-                    return 25;
-                }
-                else{
+                if (caractere == 's') {
+                    estadoAtual = 14;
+                } else {
                     return -1;
                 }
+                break;
             case 11:
-                if(caractere == 'i'){
-                    estadoAtual = 12;
-                }else{
+                if (caractere == 'i') {
+                    estadoAtual = 15;
+                } else {
                     return -1;
                 }
+                break;
             case 12:
-                if (caractere == 'l'){
-                    estadoAtual = 13;
-                }
-                else{
+                if (caractere == 'i'){
+                    estadoAtual = 16;
+                } else{
                     return -1;
                 }
+                break;
             case 13:
-                if(caractere == 'e'){
-                    return 23;
-                }
-                else{
+                if (caractere == 'a') {
+                    estadoAtual = 17;
+                } else {
                     return -1;
                 }
-            case 100: 
-                break; 
+                break;
+            case 14:
+                if (caractere == 'e') {
+                    return 22;
+                } else {
+                    return -1;
+                }
+            case 15:
+                if (caractere == 'l') {
+                    estadoAtual = 18;
+                } else {
+                    return -1;
+                }
+                break;
+            case 16:
+                if (caractere == 't') {
+                    estadoAtual = 19;
+                } else {
+                    return -1;
+                }
+                break;
+            case 17:
+                if (caractere == 't') {
+                    return 26;
+                } else {
+                    return -1;
+                }
+            case 18:
+                if (caractere == 'e') {
+                    return 23;
+                } else {
+                    return -1;
+                }
+            case 19:
+                if (caractere == 'c') {
+                    estadoAtual = 20;
+                } else {
+                    return -1;
+                }
+                break;
+            case 20:
+                if (caractere == 'h') {
+                    return 27;
+                } else {
+                    return -1;
+                }
         }
+        return -1;
     }
 }
